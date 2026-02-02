081f0e1d2bc3885ab7ccbca22b66758b85a3b279
“Akira-Taniguchi” <dededededaiou2003@yahoo.co.jp>
2026-02-02 17:21:25 +0900

    add const

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,13 +6,13 @@ import {Script} from "forge-std/Script.sol";
 import {CREATE3Factory} from "../src/CREATE3Factory.sol";
 
 contract DeployScript is Script {
+    bytes32 public constant SALT = keccak256("intmax");
     function run() public returns (CREATE3Factory factory) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
-        bytes32 salt = keccak256("intmax");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        factory = new CREATE3Factory{salt: salt}();
+        factory = new CREATE3Factory{salt: SALT}();
 
         vm.stopBroadcast();
     }
