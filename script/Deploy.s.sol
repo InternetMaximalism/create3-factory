6c95b74c697b2d678380c89743045b0e1e5a39a8
“Akira-Taniguchi” <dededededaiou2003@yahoo.co.jp>
2026-02-02 17:28:44 +0900

    add Salt sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.33;
 import {Script} from "forge-std/Script.sol";
 
 import {CREATE3Factory} from "../src/CREATE3Factory.sol";
+import {CREATE3_FACTORY_SALT} from "../src/Salt.sol";
 
 contract DeployScript is Script {
-    bytes32 public constant SALT = keccak256("intmax");
     function run() public returns (CREATE3Factory factory) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
         vm.startBroadcast(deployerPrivateKey);
 
-        factory = new CREATE3Factory{salt: SALT}();
+        factory = new CREATE3Factory{salt: CREATE3_FACTORY_SALT}();
 
         vm.stopBroadcast();
     }
